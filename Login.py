bbdf34cc3aa835f83ff8c692dbce2f47d2a7fdeb
Katiyarkarelaking <karelaking277@gmail.com>
2024-01-19 15:56:50 +0530

    All Ui Related Work Completed - 19/01/2024 - (03:56 PM)

---
+++ Login.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import customtkinter as ttk
+
+page = ttk.CTk()
+page.title("Login")
+x = page.winfo_screenwidth() // 6
+y = page.winfo_screenheight() * 0.15
+page.geometry(f"950x600+{x}+{y}")
+page.wm_iconbitmap("assets/login_icon.ico")
+page.resizable(False, False)
+page.propagate()
+
+load_img = PhotoImage(file="assets/login.png")
+img = load_img.subsample(2, 2)
+Label(page, image=load_img, width=700).pack(side=LEFT, fill=Y)
+
+
+def userfunc(e):
+    if userName.get() != "":
+        pass
+    else:
+        userName.insert(0,"Enter User Name")
+
+
+def passwordfunc(e):
+    if password.get() != "":
+        pass
+    else:
+        password.insert(0,"Enter Password")
+
+
+frame = ttk.CTkFrame(page)
+ttk.CTkLabel(frame, text="LOGIN", font=("", 45, "bold")).pack(fill=BOTH, pady=40)
+userName = ttk.CTkEntry(frame, width=350, height=45)
+userName.pack(pady=5, padx=42)
+userName.insert(0, "Enter User Name")
+userName.bind("<FocusIn>", lambda e: userName.delete(0, 'end'))
+userName.bind("<FocusOut>", userfunc)
+password = ttk.CTkEntry(frame, width=350, height=45)
+password.pack(pady=5, padx=42)
+password.insert(0, "Enter Password")
+password.bind("<FocusIn>", lambda e: password.delete(0, 'end'))
+password.bind("<FocusOut>", passwordfunc)
+
+
+def Login():
+    if userName.get() == "mradul kumar" and password.get() == "1234":
+        print("logged in")
+        userName.insert(0,"Enter User Name")
+        password.insert(0,"Enter Password")
+    elif userName.get() == "mradul kumar" and password.get() != "1234":
+        print("incorrect password")
+    elif userName.get() != "mradul kumar" and password.get() == "1234":
+        print("incorrect user name")
+    elif userName.get() != "mradul kumar" and password.get() != "1234":
+        print("incorrect user name and password")
+
+
+loginButton = ttk.CTkButton(frame, text="Login", width=350, height=45, font=("", 28, "bold"), command=Login)
+loginButton.pack(pady=20)
+
+ttk.CTkLabel(frame, text="Others").pack()
+
+load_google = PhotoImage(file='assets/google_logo.png')
+google = load_google.subsample(2, 2)
+load_github = PhotoImage(file='assets/github_logo.png')
+github = load_github.subsample(2, 2)
+
+google_Button = ttk.CTkButton(frame, image=google, text="Goggle", width=350, height=45, font=("", 28, "bold"),
+                              fg_color="white", text_color="black")
+google_Button.pack(pady=20)
+github_Button = ttk.CTkButton(frame, image=github, text="Git Hub", width=350, height=45, font=("", 28, "bold"),
+                              fg_color="white", text_color="black")
+github_Button.pack()
+
+frame.pack(side=RIGHT, fill=Y, pady=20, padx=20)
+page.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# LoginPage
-this is simple gui login page build in python language.
+
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+
+# Hi, I'm White Angel! 👋
+
+
+## 🚀 About Me
+I'm a developer and a student...
+
+
+# Unit Converter
+
+This is simple Unit Converter Application build in python programming language.
+
+
+## Installation
+
+**Install Unit Converter from Github.**
+
+Install python from https://www.python.org/downloads/
+
+Install Git from https://www.git-scm.com/downloads
+
+you can skip this step if you have already installed the python and git.
+
+
+- **clone Project to your local computer.**
+```bash
+  git clone https://github.com/Karelaking/Unit_Coverter.git
+```
+
+- **Go to the project folder**
+```bash
+cd Unit Coverter
+```
+
+- **install requirements for the unit converter**
+```bash
+pip install -r requirements.txt
+```
+
+- **Run Unit_Coverter.py**
+```bash
+python3 Unit_Coverter.py
+```
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Optimizations
+
+I will try to make it batter and more batter according to youe suggestions.
+
+## Screenshots
+
+![App Screenshot](assets/logIn_Page.png)
+
+
+## Acknowledgements
+
+ - [Git](https://www.git-scm.com/downloads)
+ - [python3](https://www.python.org/downloads/)
+ - [readme](https://readme.so/)
+ - [Firgma](https://www.figma.com/)
+ - [opensource](https://opensource.org/license/mit/)
+ - [icon-icons](https://icon-icons.com/)
+## Support
+
+For support, email karelaking277@gmail.com.
+
